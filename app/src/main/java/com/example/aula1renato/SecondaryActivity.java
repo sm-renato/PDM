63b69e6114a8318f40566429a579612e4eb5ce15
Renato Machado <renatochavecinza@gmail.com>
2021-03-07 17:04:32 -0300

    Commit do projeto base

---
+++ app/src/main/java/com/example/aula1renato/SecondaryActivity.java
@@ -0,0 +1,14 @@
+package com.example.aula1renato;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecondaryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_secondary);
+    }
+}
